dc0cd3b867d779c0abeaccbcb79b5c95ce9cc62e
root <root@ip-172-31-32-68.us-west-2.compute.internal>
2015-03-28 20:41:41 +0000

    Added mongodb update to the app. Data is now obtained from mongodb using pyMongo

--- flask_app.py
+++ flask_app.py
@@ -1,28 +1,57 @@
-from flask import Flask, request, url_for,g
-import random
-from flask.ext.sqlalchemy import SQLAlchemy
-import mysql.connector
+from flask import Flask, jsonify,request, render_template, redirect, url_for, session
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
 import json
 from bson import json_util
 from bson.json_util import dumps
-import newtrial as n
+from werkzeug import secure_filename
+from random import randint
+#from flaskext.uploads import delete, init, save, Upload
+#from flask.uploads import UploadSet, IMAGES
+import os
+
+from flask import make_response
+
+from bson.json_util import dumps
 
 
-app = Flask(__name__)
-"""
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://shridharmanvi:hireyaru@mysql.server/db'
-db = SQLAlchemy(app)
-"""
+app=Flask(__name__)
 
-app.secret_key = 'This is really unique and secret'
-
-@app.route('/')
-def hello_person():
-    return 'Hi there!!'
+app.secret_key = 'dfgsdfgsfgdfsg'
+app.config['MONGO_HOST'] = '0.0.0.0'
+app.config['MONGO_PORT'] = 27017
+app.config['MONGO_DBNAME'] = 'se'
+#app.config['MONGO_USERNAME'] = ''
+#app.config['MONGO_PASSWORD'] = ''
+mongo = PyMongo(app)
 
 
-@app.route('/<search_key>')
-def show_entries(search_key):
-    key=search_key
-    y=n.trial(key)
-    return y
+def replace_value_with_definition(dictionary,key_to_find, definition):
+    for key in dictionary.keys():
+        if key == key_to_find:
+            dictionary[key] = definition
+
+
+@app.route('/findout')
+def loggedout():
+    #data = mongo.db.tweets.find({"one":{ '$regex' : email}})
+    data = mongo.db.tweets.find()
+
+    j=[]
+    i={"one":1,"two":2}
+    for x in data:
+        replace_value_with_definition(i,'one',x['one'])
+        replace_value_with_definition(i,'two',x['two'])
+        j.append(i)
+        i={"one":1,"two":2}
+        #print j
+     
+    k=json.dumps(j)
+    return k
+
+
+
+
+if __name__== '__main__':
+    app.run(host='0.0.0.0', port=80,debug='true')
+
